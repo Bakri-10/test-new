2368d814b2d6a2ecf4e4e088279e5ed40b23603b
Bakri-10 <>
2025-04-18 20:55:56 +0100

    new

--- windows-vm/import/main.tf
+++ windows-vm/import/main.tf
@@ -61,61 +61,70 @@ locals {
 # Data sources
 data "azurerm_subscription" "current" {}
 
-# Use azurerm_resources instead of azurerm_virtual_machine to get all VM details
-data "azurerm_resources" "import_vm" {
+# Get the resource group first to validate it exists
+data "azurerm_resource_group" "target_rg" {
+  provider = azurerm.adt
+  name     = local.rg_name
+}
+
+# Get VM details using a more resilient approach
+data "azurerm_virtual_machine" "existing" {
   provider            = azurerm.adt
   name                = local.vm_name
   resource_group_name = local.rg_name
-  type                = "Microsoft.Compute/virtualMachines"
-  required_tags       = {}
 }
 
+# Get OS disk details if it exists
 data "azurerm_managed_disk" "os_disk" {
   provider            = azurerm.adt
+  count               = try(data.azurerm_virtual_machine.existing.id != "", 0) == 1 ? 1 : 0
   name                = local.os_disk_name
   resource_group_name = local.rg_name
 }
 
+# Get data disk details if they exist
 data "azurerm_managed_disk" "data_disks" {
   provider            = azurerm.adt
-  count               = local.data_disk_count
-  name                = "${local.vm_name}-data_disk-${count.index}"
+  for_each           = try(data.azurerm_virtual_machine.existing.id != "", false) ? {
+    for idx in range(local.data_disk_count) : tostring(idx) => idx
+  } : {}
+  name                = "${local.vm_name}-data_disk-${each.value}"
   resource_group_name = local.rg_name
 }
 
+# Get NIC details if they exist
 data "azurerm_network_interface" "nics" {
   provider            = azurerm.adt
-  count               = local.nic_count
-  name                = "${local.vm_name}-nic-${format("%02d", count.index + 1)}"
+  for_each           = try(data.azurerm_virtual_machine.existing.id != "", false) ? {
+    for idx in range(1, local.nic_count + 1) : tostring(idx) => idx
+  } : {}
+  name                = "${local.vm_name}-nic-${format("%02d", each.value)}"
   resource_group_name = local.rg_name
 }
 
 # Import placeholder resources
-# The actual import will happen via the terraform import command in the workflow
 resource "azurerm_windows_virtual_machine" "import" {
   provider            = azurerm.adt
   name                = local.vm_name
   resource_group_name = local.rg_name
-  location            = try(data.azurerm_resources.import_vm.resources[0].location, var.location)
-  size                = "Standard_D2s_v3" # Using a default size, will be overridden by import
+  location            = data.azurerm_resource_group.target_rg.location
+  size                = try(data.azurerm_virtual_machine.existing.size, "Standard_D2s_v3")
   
-  # These values will be updated during import but are required for the resource declaration
+  # These values will be updated during import
   admin_username      = "placeholder"
-  admin_password      = "placeholder123!" # Must meet complexity requirements (placeholder only)
+  admin_password      = "placeholder123!"
   
-  # Use placeholder network interface IDs - these will be updated during import
-  network_interface_ids = try(
-    [for nic in data.azurerm_network_interface.nics : nic.id],
-    ["/subscriptions/${local.naming.subscription_id}/resourceGroups/${local.rg_name}/providers/Microsoft.Network/networkInterfaces/${local.vm_name}-nic-01"]
-  )
+  # Use dynamic network interface IDs
+  network_interface_ids = [
+    for nic in data.azurerm_network_interface.nics : nic.id
+  ]
   
   os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = try(data.azurerm_managed_disk.os_disk.storage_account_type, "Standard_LRS")
+    caching              = try(data.azurerm_managed_disk.os_disk[0].disk_access_id != null, true) ? "ReadWrite" : "None"
+    storage_account_type = try(data.azurerm_managed_disk.os_disk[0].storage_account_type, "Standard_LRS")
     name                 = local.os_disk_name
   }
   
-  # Placeholder source image reference - will be updated during import
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
@@ -123,25 +132,26 @@ resource "azurerm_windows_virtual_machine" "import" {
     version   = "latest"
   }
   
-  # This resource is just a placeholder for the import command
-  # The actual values will be populated once the import is complete
   lifecycle {
     ignore_changes = all
   }
 }
 
-# Network interface placeholders
+# Network interface placeholders with dynamic configuration
 resource "azurerm_network_interface" "import" {
   provider            = azurerm.adt
-  count               = local.nic_count
-  name                = data.azurerm_network_interface.nics[count.index].name
-  location            = data.azurerm_network_interface.nics[count.index].location
-  resource_group_name = data.azurerm_network_interface.nics[count.index].resource_group_name
+  for_each           = data.azurerm_network_interface.nics
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
   
-  ip_configuration {
-    name                          = "${local.vm_name}-ipconfig"
-    subnet_id                     = data.azurerm_network_interface.nics[count.index].ip_configuration[0].subnet_id
-    private_ip_address_allocation = "Dynamic"
+  dynamic "ip_configuration" {
+    for_each = each.value.ip_configuration
+    content {
+      name                          = ip_configuration.value.name
+      subnet_id                     = ip_configuration.value.subnet_id
+      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation
+    }
   }
   
   lifecycle {
@@ -149,29 +159,29 @@ resource "azurerm_network_interface" "import" {
   }
 }
 
-# Data disk placeholders
+# Data disk placeholders with dynamic configuration
 resource "azurerm_managed_disk" "import" {
   provider             = azurerm.adt
-  count                = local.data_disk_count
-  name                 = data.azurerm_managed_disk.data_disks[count.index].name
-  location             = data.azurerm_managed_disk.data_disks[count.index].location
-  resource_group_name  = data.azurerm_managed_disk.data_disks[count.index].resource_group_name
-  storage_account_type = data.azurerm_managed_disk.data_disks[count.index].storage_account_type
+  for_each            = data.azurerm_managed_disk.data_disks
+  name                 = each.value.name
+  location             = each.value.location
+  resource_group_name  = each.value.resource_group_name
+  storage_account_type = each.value.storage_account_type
   create_option        = "Empty"
-  disk_size_gb         = data.azurerm_managed_disk.data_disks[count.index].disk_size_gb
+  disk_size_gb         = each.value.disk_size_gb
   
   lifecycle {
     ignore_changes = all
   }
 }
 
-# VM data disk attachment placeholders
+# VM data disk attachment placeholders with dynamic configuration
 resource "azurerm_virtual_machine_data_disk_attachment" "import" {
   provider           = azurerm.adt
-  count              = local.data_disk_count
-  managed_disk_id    = azurerm_managed_disk.import[count.index].id
+  for_each          = data.azurerm_managed_disk.data_disks
+  managed_disk_id    = azurerm_managed_disk.import[each.key].id
   virtual_machine_id = azurerm_windows_virtual_machine.import.id
-  lun                = count.index
+  lun                = tonumber(each.key)
   caching            = "ReadWrite"
   
   lifecycle {
@@ -179,19 +189,25 @@ resource "azurerm_virtual_machine_data_disk_attachment" "import" {
   }
 }
 
-# Output the import commands for reference
+# Output the import commands
 output "vm_import_command" {
   value = "terraform import azurerm_windows_virtual_machine.import ${local.vm_id}"
 }
 
 output "nic_import_commands" {
-  value = [for i, nic_id in local.nic_ids : "terraform import azurerm_network_interface.import[${i}] ${nic_id}"]
+  value = [for nic_id in local.nic_ids : 
+    "terraform import 'azurerm_network_interface.import[\"${split("/", nic_id)[8]}\"]' ${nic_id}"
+  ]
 }
 
 output "data_disk_import_commands" {
-  value = [for i, disk_id in local.data_disk_ids : "terraform import azurerm_managed_disk.import[${i}] ${disk_id}"]
+  value = [for disk_id in local.data_disk_ids :
+    "terraform import 'azurerm_managed_disk.import[\"${split("/", disk_id)[8]}\"]' ${disk_id}"
+  ]
 }
 
 output "data_disk_attachment_import_commands" {
-  value = [for i in range(0, local.data_disk_count) : "terraform import azurerm_virtual_machine_data_disk_attachment.import[${i}] ${azurerm_windows_virtual_machine.import.id}|${azurerm_managed_disk.import[i].id}|${i}"]
+  value = [for i in range(0, local.data_disk_count) : 
+    "terraform import 'azurerm_virtual_machine_data_disk_attachment.import[\"${i}\"]' ${azurerm_windows_virtual_machine.import.id}|${local.data_disk_ids[i]}|${i}"
+  ]
 } 
